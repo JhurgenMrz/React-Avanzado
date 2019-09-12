603f6894bdd21ae426d7793184248d07f8854fb2
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-09-12 18:32:42 -0300

    Usamos info real para las categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Category} from '../Category'
 import {Item, List} from '../ListOfCategories/styles'
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = ()=>{
     return(
         <List>
             {
-                [1,2,3,4,5,6,7,8,9].map((category)=>(
-                    <Item key={category}>
-                        <Category />
+                categories.map((category)=>(
+                    <Item key={category.id}>
+                        <Category {...category}/>
                     </Item>
                 ))
             }
