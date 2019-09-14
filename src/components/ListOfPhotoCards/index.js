8142c54f37b11d2d11a5fc458b5fa21a869cf3b0
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-09-14 16:27:16 -0300

    CustomHooks

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -5,8 +5,8 @@ export const ListOfPhotoCards = ()=>{
     return (
         <ul>
             {
-                [1,2,3,4,5,6,7].map((card,index)=>(
-                        <PhotoCart key={card}/>
+                [1,2,3,4,5,6,7].map((id)=>(
+                        <PhotoCart key={id} id={id}/>
                 ))
             }
         </ul>
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,28 +1,21 @@
 import React, {Fragment,useEffect, useRef,useState} from 'react'
 import {ImgWrapper,Img, Button,Article} from './styles'
-import {MdFavoriteBorder} from 'react-icons/md'
+import {MdFavoriteBorder, MdFavorite} from 'react-icons/md'
+import {useLocalStorage} from '../../hooks/useLocalStorage'
+import {useNearScreen} from '../../hooks/useNearScreen'
+
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
+
 export const PhotoCart = ({id, likes = 0, src = DEFAULT_IMAGE }) =>{
 
-    const [show, setShow] = useState(false)
+    const [show, element ] = useNearScreen()
 
-    const element = useRef(null)
+    const key = `like-${id}`
+    const [liked, setLiked] = useLocalStorage(key, false)
 
-    useEffect(function(){
-        const observer = new window.IntersectionObserver(function(entries){
-            const { isIntersecting } = entries[0]
-            // isIntersecting es para saber si esta en el viewPort
-            if(isIntersecting){
-                console.log('Si!')
-                setShow(true)
-                observer.disconnect()
-            }})
-
-        //Iniciamos el observador con el elemento
-        observer.observe(element.current)
-    },[element])
+    const Icon = liked ? MdFavorite : MdFavoriteBorder
 
     return (
         <Article ref={element}>
@@ -35,8 +28,8 @@ export const PhotoCart = ({id, likes = 0, src = DEFAULT_IMAGE }) =>{
                         <Img src={src} />
                     </ImgWrapper>
                     </a>
-                    <Button>
-                    <MdFavoriteBorder size={`32px`}/>
+                    <Button onClick={ () => setLiked(!liked) }>
+                    <Icon size={`32px`} color={`red`}/>
                     {likes} likes!
                     </Button>
                 </Fragment>
